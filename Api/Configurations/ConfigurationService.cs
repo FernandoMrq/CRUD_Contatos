5347e87fd144e0095d4e9626615b6a3537fdcc60
Fernando Marques <frndoliveira@gmail.com>
2022-12-19 14:53:39 -0300

    Add project files.

---
+++ Api/Configurations/ConfigurationService.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Api.Configurations
+{
+    [ExcludeFromCodeCoverage]
+    public static class ConfigurationService
+    {
+        public static IServiceCollection AddAplications(this IServiceCollection services)
+        {
+            return services;
+        }
+
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            return services;
+        }
+
+        public static IServiceCollection AddBusiness(this IServiceCollection services)
+        {
+            return services;
+        }
+
+        public static IServiceCollection AddRules(this IServiceCollection services)
+        {
+            return services;
+        }
+
+        public static IServiceCollection AddExternalServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+
+        public static IServiceCollection AddInfra(this IServiceCollection services)
+        {
+            return services;
+        }
+
+        public static IServiceCollection AddCache(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+
+        public static IServiceCollection AddConsumers(this IServiceCollection services)
+        {
+            return services;
+        }
+
+        public static IServiceCollection AddSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen();
+
+            return services;
+        }
+    }
+}
